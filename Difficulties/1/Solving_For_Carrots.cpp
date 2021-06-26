9885e2ba0a01eaac2122ad76236875bb43ff76a8
BC46 <cs.melis@student.avans.nl>
2021-06-26 11:14:42 +0200

    Add Solving For Carrots solution

---
+++ Difficulties/1/Solving_For_Carrots.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int contestants, solvedProblems;
+
+    std::cin >> contestants >> solvedProblems;
+
+    cout << solvedProblems;
+
+    return 0;
+}
