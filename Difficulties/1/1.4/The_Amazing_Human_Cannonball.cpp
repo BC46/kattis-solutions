c56884623b32b51f2d0bc2010c8508cbe82f69b0
BC46 <cs.melis@student.avans.nl>
2021-07-03 17:49:07 +0200

    Add solution for The Amazing Human Cannonball

---
+++ Difficulties/1/1.4/The_Amazing_Human_Cannonball.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    for (size_t i = 0; i < n; i++) {
+        std::string result = "Safe";
+        float v, angle, x1, h1, h2;
+        std::cin >> v >> angle >> x1 >> h1 >> h2;
+
+        float radian = angle / 180 * M_PI;
+        float t = x1 / (cos(radian) * v);
+        float y = v * t * sin(radian) - 0.5 * 9.81 * pow(t, 2);
+
+        std::cout << (y >= h1 + 1 && y <= h2 - 1 ? "Safe" : "Not Safe") << std::endl;
+    }
+
+    return 0;
+}
