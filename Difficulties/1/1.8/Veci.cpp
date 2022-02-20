306a25356e7d78de6ccc0819455e2958bc45de34
C.S. Melis <cees3331x00@gmail.com>
2022-02-20 22:37:21 +0100

    Add correct Veci solution

--- Difficulties/1/1.8/Veci.cpp
+++ Difficulties/1/1.8/Veci.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
-#include <set>
+#include <map>
 
 int main()
 {
     int x, maxValue, result(0);
     std::string xString, temp;
-    std::set<char> xSet;
+    std::map<char, int> xMap;
 
     std::cin >> xString;
 
     for (char &number : xString) {
         temp += "9";
-        xSet.insert(number);
+
+        if (xMap.find(number) == xMap.end()) {
+            xMap.insert(std::pair<char, int>(number, 0));
+        } else {
+            xMap[number]++;
+        }
     }
 
     x = stoi(xString) + 1;
@@ -19,15 +24,40 @@ int main()
 
     for (; x <= maxValue; ++x) {
         temp = std::to_string(x);
-        std::set<char> tempSet;
+        std::map<char, int> tempMap;
+
+        bool hasSameCharacters{true};
 
         for (const auto &tempLetter : temp) {
-            tempSet.insert(tempLetter);
+            if (tempMap.find(tempLetter) == tempMap.end()) {
+                if (xMap.find(tempLetter) == xMap.end()) {
+                    hasSameCharacters = false;
+                    break;
+                }
+
+                tempMap.insert(std::pair<char, int>(tempLetter, 0));
+            } else {
+                tempMap[tempLetter]++;
+            }
         }
 
-        if (tempSet == xSet) {
-            result = x;
-            break;
+        if (hasSameCharacters) {
+            bool hasSameOccurrences{true};
+
+            for (const auto &key : xMap) {
+                int xOccurrences = xMap.find(key.first)->second;
+                int tempOccurrences = tempMap.find(key.first)->second;
+
+                if (xOccurrences != tempOccurrences) {
+                    hasSameOccurrences = false;
+                    break;
+                }
+            }
+
+            if (hasSameOccurrences) {
+                result = x;
+                break;
+            }
         }
     }
 
