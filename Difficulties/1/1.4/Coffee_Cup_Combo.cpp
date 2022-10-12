ff14b27c1bd432c3d23e74688aa9167b6c528f3f
C.S. Melis <cees3331x00@gmail.com>
2022-10-12 17:22:49 +0200

    Add Coffee Cup Combo solution

---
+++ Difficulties/1/1.4/Coffee_Cup_Combo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    std::string input;
+    int n, coffeeCount = 0, awakeLectures = 0;
+    bool hasCoffee;
+
+    std::cin >> n >> input;
+    for (const auto &coffee : input) {
+        if (coffee == '1')
+            coffeeCount = 2;
+
+        if (coffeeCount > 0) {
+            --coffeeCount;
+            ++awakeLectures;
+        }
+
+        if (coffee == '1')
+            coffeeCount = 2;
+    }
+
+    std::cout << awakeLectures << std::endl;
+    return 0;
+}
