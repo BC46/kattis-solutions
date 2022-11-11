c348c3fc9379ccb0686e7854f78cdd907cc72fc0
C.S. Melis <cees3331x00@gmail.com>
2022-11-11 12:21:31 +0100

    Remove One Chicken shortest solution

--- src/onechicken_shortest.cpp
+++
@@ -1,2 +0,0 @@
-#include<iostream>
-using namespace std;int main(){int n,m;cin>>n>>m;m-=n;string s=to_string(abs(m)),c="Dr. Chaz ",k=" of chicken";cout<<(m>=0?c+"will have "+s+" piece"+(m==1?"":"s")+k+" left over!":c+"needs "+s+" more piece"+(s=="1"?"":"s")+k+"!");}
