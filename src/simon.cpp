ef3b60527769067b48f55f513aeb9dc7423c17a8
C.S. Melis <cees3331x00@gmail.com>
2023-06-08 23:47:29 +0200

    Add Simon Says solution'

---
+++ src/simon.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string input;
+
+    int t;
+    cin >> t;
+
+    cin.ignore();
+
+    while (t--) {
+        getline(cin, input);
+
+        if (input.find("simon says") == 0 && input.size() > 10)
+            cout << input.substr(11) << endl;
+
+        cout << '\n';
+    }
+
+    return 0;
+}
