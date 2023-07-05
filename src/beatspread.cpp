4b8b7a6f0b66ff7c96c94e4c195e074fb9a64622
C.S. Melis <cees3331x00@gmail.com>
2023-07-05 09:36:51 +0200

    Update Beat the Spread! solution

--- src/beatspread.cpp
+++ src/beatspread.cpp
@@ -12,7 +12,7 @@ int main()
         cin >> s >> d;
 
         s1 = (s - d) / 2;
-        s2 = d + (s - d) / 2;
+        s2 = d + s1;
 
         if (s1 + s2 == s && s1 >= 0 && s2 >= 0)
             cout << max(s1, s2) << ' ' << min(s1, s2) << '\n';
