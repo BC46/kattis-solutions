63122c007375464edf3e19d6a022829d7a85fa88
C.S. Melis <cees3331x00@gmail.com>
2023-07-07 23:23:59 +0200

    Add Arithmetic Functions solution

---
+++ src/arithmeticfunctions.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include "arithmeticfunctions.h"
+
+// Compute x^3
+int cube(int x){
+    return pow(x, 3);
+}
+
+// Compute the maximum of x and y
+int max(int x, int y){
+    return std::max(x, y);
+}
+
+// Compute x - y
+int difference(int x, int y){
+    return x - y;
+}
