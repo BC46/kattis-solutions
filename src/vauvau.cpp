45ab159b854d2f9fe82cb3ea60e1c85bc33f22f1
C.S. Melis <cees3331x00@gmail.com>
2023-08-31 19:46:41 +0200

    Update Vauvau solution

--- src/vauvau.cpp
+++ src/vauvau.cpp
@@ -13,21 +13,18 @@ bool willAttack(int aggressiveTime, int calmTime, int arrivalTime) {
 
 int main()
 {
+    string results[3] = { "none", "one", "both" };
+
     int a, b, c, d;
     cin >> a >> b >> c >> d;
 
     int arrivalTimes[3];
     cin >> arrivalTimes[0] >> arrivalTimes[1] >> arrivalTimes[2];
 
-    for (auto &arrivalTime : arrivalTimes) {
+    for (const auto &arrivalTime : arrivalTimes) {
         int attacks = (int) willAttack(a, b, arrivalTime) + (int) willAttack(c, d, arrivalTime);
 
-        if (attacks == 0)
-            cout << "none\n";
-        else if (attacks == 1)
-            cout << "one\n";
-        else
-            cout << "both\n";
+        cout << results[attacks] << '\n';
     }
 
     return 0;
