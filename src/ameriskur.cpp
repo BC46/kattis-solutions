204e03296fe20dc8e9b486dd9161c0b4dbe1cde5
C.S. Melis <cees3331x00@gmail.com>
2023-08-31 17:23:15 +0200

    Add Amerískur vinnustaður solution

---
+++ src/ameriskur.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double n;
+    cin >> n;
+
+    cout << setprecision(20) << n * 0.09144 << '\n';
+
+    return 0;
+}
