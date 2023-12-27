f833c93c3c0e61eb040c7f9f35ff16ce05bf2d1c
C.S. Melis <cees3331x00@gmail.com>
2023-12-27 16:33:20 +0100

    Add Alien Numbers solution

---
+++ src/aliennumbers.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int tn = 1; tn <= t; ++tn) {
+        string alien_number, source_language, target_language, result;
+        cin >> alien_number >> source_language >> target_language;
+
+        long long number_value = 0, digits = alien_number.length();
+
+        for (char i : alien_number) {
+            number_value += source_language.find(i) * pow(source_language.length(), digits - 1);
+            --digits;
+        }
+
+        long long test = number_value, digit_amount = 0;
+
+        for (long long i = 0; test > i;) {
+            ++digit_amount;
+            i = pow(target_language.length(), digit_amount) - 1;
+        }
+
+        if (digit_amount == 0)
+            result += target_language[0];
+
+        for (int i = digit_amount; i > 0; --i) {
+            for (int j = target_language.length() - 1; j >= 0; --j) {
+                long long val = j * pow(target_language.length(), i - 1);
+
+                if (val <= number_value) {
+                    number_value -= val;
+                    result.push_back(target_language[j]);
+                    break;
+                }
+            }
+        }
+
+        cout << "Case #" << tn << ": " << result << '\n';
+    }
+
+    return 0;
+}
