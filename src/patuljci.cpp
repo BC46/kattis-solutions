a9fed11af1de5e42251bcf39f67ed9566fe3d01e
C.S. Melis <cees3331x00@gmail.com>
2024-01-07 22:14:29 +0100

    Update Patuljci solution

--- src/patuljci.cpp
+++ src/patuljci.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-vector<int> dwarves(9);
-
-int countWithSkips(int skip1, int skip2) {
-    int result = 0;
-
-    for (int i = 0; i < 9; ++i) {
-        if (i != skip1 && i != skip2)
-            result += dwarves[i];
-    }
-
-    return result;
-}
-
 int main()
 {
-    for (auto &d : dwarves)
+    int dwarves[9], total = 0;
+
+    for (auto &d : dwarves) {
         cin >> d;
+        total += d;
+    }
 
     for (int i = 0; i < 8; ++i) {
         for (int j = i + 1; j < 9; ++j) {
-            if (countWithSkips(i, j) == 100) {
-                for (int k = 0; k < 9; ++k) {
-                    if (i != k && j != k)
-                        cout << dwarves[k] << '\n';
-                }
+            if (total - dwarves[i] - dwarves[j] != 100)
+                continue;
 
-                return 0;
+            for (int k = 0; k < 9; ++k) {
+                if (i != k && j != k)
+                    cout << dwarves[k] << '\n';
             }
+
+            return 0;
         }
     }
 }
