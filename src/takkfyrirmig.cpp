2d25a0f5e3f3d959f22d1fd1c495d9ee26d94cb6
C.S. Melis <cees3331x00@gmail.com>
2024-06-09 16:58:13 +0200

    Add Takk fyrir mig solution

---
+++ src/takkfyrirmig.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    string name;
+
+    cin >> n;
+
+    while (n--) {
+        cin >> name;
+        cout << "Takk " << name << '\n';
+    }
+
+    return 0;
+}
