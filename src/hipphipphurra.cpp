a92ae50272d66ba4970cbb13aa6f07a3c11a7689
C.S. Melis <cees3331x00@gmail.com>
2024-06-09 17:18:40 +0200

    Add Hipp Hipp Húrra solution

---
+++ src/hipphipphurra.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int n;
+
+    cin >> name >> n;
+
+    while (n--)
+        cout << "Hipp hipp hurra, " << name << "!\n";
+
+    return 0;
+}
